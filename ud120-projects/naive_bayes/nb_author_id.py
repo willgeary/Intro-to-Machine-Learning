d9c077f05baf1cdfb0cde6b0740c46c05e9ee79f
willgeary <willcgeary@gmail.com>
2015-11-28 19:18:52 -0500

    Completed mini project #1. Create NB classifier, calulate the accuracy and time it.

--- ud120-projects/naive_bayes/nb_author_id.py
+++ ud120-projects/naive_bayes/nb_author_id.py
@@ -14,6 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -21,11 +23,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()  t1, 3), "s"
+
+print pred
+
+print accuracy_score(pred, labels_test)
 
 
 #########################################################
