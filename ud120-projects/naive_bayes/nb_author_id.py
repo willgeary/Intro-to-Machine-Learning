b9fb1ba4aa3c7b6fb87f52379a69f599ac58d56f
willgeary <willcgeary@gmail.com>
2015-11-30 18:21:21 -0500

    Mini project #1. Updated Naive Bayes

--- ud120-projects/naive_bayes/nb_author_id.py
+++ ud120-projects/naive_bayes/nb_author_id.py
@@ -31,6 +31,7 @@ t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time() - t0, 3), "s"
 
+
 t1 = time()
 pred = clf.predict(features_test)
 print "predicting time:", round(time() - t1, 3), "s"
