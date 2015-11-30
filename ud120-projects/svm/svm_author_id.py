930a6ad47823a9002e7cc51eb21c7b071f752e93
willgeary <willcgeary@gmail.com>
2015-11-30 18:09:31 -0500

    Mini project #2: Updated SVM

--- ud120-projects/svm/svm_author_id.py
+++ ud120-projects/svm/svm_author_id.py
@@ -24,7 +24,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+clf = SVC(kernel="rbf", C=10000.0)
+t0 = time()
+
+# Speed up algorithm by training it on a smaller test set
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time() - t1, 3), "s"
+
+print pred
+print accuracy_score(pred, labels_test)
+
+print "Count for 1 : ", list(pred).count(1)
 #########################################################
 
 
